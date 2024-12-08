562098faadbc7336c58c473f8293e79cde3572ce
Mohamed Thalib <114242+thalib@users.noreply.github.com>
2024-12-08 19:12:12 +0530

    now use bootstrap as nodejs package

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,2 @@
-import './bootstrap';
+import 'bootstrap/js/index.esm.js';
+import './custom';
--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,14 +1,10 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>{{ config('app.name') }}</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
 <!-- https://laravel.com/docs/11.x/vite#loading-your-scripts-and-styles -->
-@vite(['resources/css/app.css', 'resources/js/app.js'])
+@vite(['resources/css/app.css'])
 
 @stack('scriptsHead')
 
