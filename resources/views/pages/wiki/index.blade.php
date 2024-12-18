d7b81950b993fb96f6d6c9c1e46974ca048030af
Mohamed Thalib <114242+thalib@users.noreply.github.com>
2024-12-18 19:57:53 +0530

    If content repo not initialized, then show message in frontend

--- resources/views/pages/wiki/index.blade.php
+++ resources/views/pages/wiki/index.blade.php
@@ -44,23 +44,29 @@
         </div>
     </div>
     <div class="row pt-4">
-        @foreach ($dirs as $dir => $files)
-            <div class="col-12 col-md-6">
-                <div class="card text-bg-dark mb-3 shadow">
-                    <div class="card-header">
-                        {{ $dir }}
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        @foreach ($files as $file)
-                            <li class="list-group-item">
-                                <a class="link-dark link-offset-3"
-                                    href="{{ route('wiki.page', ['any' => $file['url']]) }}">{{ $file['title'] }}</a>
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
+        @if (empty($dirs))
+            <div class="alert alert-danger">
+                Let's Get Started! Your Content Repository Needs Setup
             </div>
-        @endforeach
+        @else
+            @foreach ($dirs as $dir => $files)
+                <div class="col-12 col-md-6">
+                    <div class="card text-bg-dark mb-3 shadow">
+                        <div class="card-header">
+                            {{ $dir }}
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            @foreach ($files as $file)
+                                <li class="list-group-item">
+                                    <a class="link-dark link-offset-3"
+                                        href="{{ route('wiki.page', ['any' => $file['url']]) }}">{{ $file['title'] }}</a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            @endforeach
+        @endif
     </div>
 @stop
 
